259277022af2bda4918e6305889353657ab6f9b8
Yash Hegde <yash4success@gmail.com>
2024-07-14 11:56:19 +0530

    navbar

---
+++ components/Navbar/NavbarComponent.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import logo from "../../public/image.png";
+import Link from "next/link";
+import { DiscordLogoIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+export default function NavbarComponent() {
+  return (
+    <div className="flex justify-around items-center border-b border-zinc-400 p-3 bg-amber-50 shadow-md">
+      <div>
+        <Image alt="logo" src={logo} className="h-14 w-16 border-zinc-400" />
+      </div>
+      <div className="flex space-x-4 border rounded-2xl">
+        <div className="hover:border-zinc-400 border-transparent border w-14 text-center p-2 rounded-2xl hover:bg-amber-100">
+          <Link href={"https://khoj.dev/whatsapp"}>Docs</Link>
+        </div>
+        <div className="hover:border-zinc-400 border-transparent border w-18 text-center p-2 rounded-2xl hover:bg-amber-100">
+          <Link href={"https://khoj.dev/pricing"}>Pricing</Link>
+        </div>
+        <div className="hover:border-zinc-400 border-transparent border w-18 text-center p-2 rounded-2xl hover:bg-amber-100">
+          <Link href={"https://khoj.dev/whatsapp"}>Whatsapp</Link>
+        </div>
+        <div className="hover:border-zinc-400 border-transparent border w-14 text-center p-2 rounded-2xl hover:bg-amber-100">
+          <Link href={"https://blog.khoj.dev/"}>Blog</Link>
+        </div>
+      </div>
+
+      <div className="flex space-x-14">
+        <div>
+          <Link href={"https://discord.com/invite/BDgyabRM6e"}>
+            <DiscordLogoIcon className="h-8 w-8" />
+          </Link>
+        </div>
+        <div className="group text-center">
+          <Link
+            className="flex space-x-1 items-center"
+            href={"https://github.com/khoj-ai/khoj"}
+          >
+            <GitHubLogoIcon className="h-8 w-8" />
+            <div className="w-16 group-hover:hidden">12k</div>
+            <div className="w-16 hidden group-hover:block text-sm">Star us</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
