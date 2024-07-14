42019d43adc93c6b28cec956fbc7bc9065ddbb34
Yash Hegde <yash4success@gmail.com>
2024-07-14 12:26:37 +0530

    featured section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import { Button } from "@/components/ui/button";
+import YCFeaturedComponent from "@/components/YCFeatured/YCFeaturedComponent";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex justify-center">
+      <div>
+        <YCFeaturedComponent />
+      </div>
+    </div>
+  );
 }
---
+++ components/YCFeatured/YCFeaturedComponent.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import YC from "../../public/YC.png";
+import Link from "next/link";
+export default function YCFeaturedComponent() {
+  return (
+    <div className="h-34 border border-orange-600 p-2 rounded-lg mt-5 font-bold text-orange-600">
+      <Link
+        href={
+          "https://www.linkedin.com/feed/update/urn:li:activity:7203452073185542144/"
+        }
+      >
+        <div className="flex justify-between items-center">
+          <div className="text-xs">FEATURED ON</div>
+          <div className="h-auto w-32">
+            <Image src={YC} alt="YC" />
+          </div>
+        </div>
+
+        <div className="text-center">AS</div>
+        <div>&quot;TOP 100 OPEN SOURCE STARTUPS&quot;</div>
+      </Link>
+    </div>
+  );
+}
+/*  */
