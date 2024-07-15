66ecd85fa34c8c2fb2d2c6d87ef2f0e26102db32
Yash Hegde <yash4success@gmail.com>
2024-07-15 06:48:50 +0530

    jump to context section bug fixed

--- components/JumpToContextSection/JumpToContextSection.tsx
+++ components/JumpToContextSection/JumpToContextSection.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import KhojScreen from "../../public/khojScreen.png";
 export default function JumpToContextSection() {
   return (
-    <div className="flex justify-center bg-amber-50 h-">
-      <div className="pt-10">
+    <div className="flex justify items-center bg-amber-50">
+      <div className="pt-10 ">
         <div className="text-5xl font-bold text-center">
           Jump straight into context
         </div>
-        <div className="flex h-auto">
-          <div className="mt-16 ml-16 mr-16 h-auto">
-            <Image src={KhojScreen} alt="khojscreen" className="h-2/3" />
+        <div className="flex justify-center">
+          <div className="mt-16 ml-16 mr-16 ">
+            <Image src={KhojScreen} alt="khojscreen" className="h-96" />
           </div>
-          <div className="text-start text-3xl w-1/2 mt-16 h-auto ">
+          <div className="text-start text-3xl w-1/2 mt-16 align-middle flex flex-col justify-center ">
             <div>
               Khoj allows you to leverage AI to make better sense of your notes
               and documents.
