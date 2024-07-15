080e73450446c91d2864361cdd3d00c8454e1aca
Yash Hegde <yash4success@gmail.com>
2024-07-15 15:18:27 +0530

    insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavbarComponent from "@/components/Navbar/NavbarComponent";
 import Footer from "@/components/Footer/Footer";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
