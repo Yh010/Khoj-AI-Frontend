7a53996390fed90af051a45a6765965fb3e9ed83
Yash Hegde <yash4success@gmail.com>
2024-07-15 15:37:37 +0530

    minor

--- components/HeaderSection/HeaderSectionComponent.tsx
+++ components/HeaderSection/HeaderSectionComponent.tsx
@@ -7,8 +7,6 @@ import CompaniesMarquee from "./CompaniesMarquee";
 export default function HeaderSectionComponent() {
   return (
     <div className="relative space-y-20 text-center">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
-
       <div className="flex justify-center ">
         <YCFeaturedComponent />
       </div>
